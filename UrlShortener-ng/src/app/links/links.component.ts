5ca75201f570b065acae81ced09fd03464ebf1ec
Fabio Lehmann <fabio.lehmann@sib.swiss>
2018-10-11 15:22:08 +0200

    add title

--- UrlShortener-ng/src/app/links/links.component.ts
+++ UrlShortener-ng/src/app/links/links.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./links.component.css']
 })
 export class LinksComponent implements OnInit {
-
+ title = 'UrlShortener-ng';
+	
   constructor() { }
 
   ngOnInit() {
